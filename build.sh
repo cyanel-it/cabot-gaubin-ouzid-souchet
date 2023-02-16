2ab74fe1ef4ad23d0b4a17fd830aae8d663e2c60
Julie SOUCHET <90184650+SouchetJulie@users.noreply.github.com>
2023-02-16 11:26:44 +0100

    fix connexion proxy-server (#7)

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Let's build goland"
-minikube image build -t golang-project ./docker/back
-kubectl apply -f ./manifests/back/golang-deployment.yaml
-kubectl apply -f ./manifests/back/golang-service.yaml
+echo "Let's build it!"
 
 kubectl apply -f ./manifests/mysql/mysql-volume.yaml
 kubectl apply -f ./manifests/mysql/mysql.yaml
+
+minikube image build -t golang-project ./docker/back
+kubectl apply -f ./manifests/back/golang.yaml
 
 kubectl apply -f ./manifests/proxy.yaml
--- clean.sh
+++ clean.sh
@@ -2,10 +2,11 @@
 
 echo "Let's clean up our environment"
 
-kubectl delete -f manifests/back/golang-deployment.yaml
-kubectl delete -f manifests/back/golang-service.yaml
-
 kubectl delete -f manifests/mysql/mysql.yaml
 kubectl delete -f manifests/mysql/mysql-volume.yaml
 
+kubectl delete -f manifests/back/golang.yaml
+
 kubectl delete -f manifests/proxy.yaml
+
+echo "Don't forget to clean /mnt/data in minikube"
